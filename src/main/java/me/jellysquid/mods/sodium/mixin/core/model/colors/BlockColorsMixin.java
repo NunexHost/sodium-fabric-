c54140bc0930612551f52fd4b41661fdca7cf1f2
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-24 15:32:23 -0300

    Update BlockColorsMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/model/colors/BlockColorsMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/model/colors/BlockColorsMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
