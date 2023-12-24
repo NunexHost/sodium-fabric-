ab1d01c4fb03c879f558688fa2fc21fce8e3fcaf
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-24 14:36:32 -0300

    Update BufferBuilderStorageMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/BufferBuilderStorageMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/BufferBuilderStorageMixin.java
@@ -5,12 +5,21 @@ import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.chunk.BlockBufferBuilderPool;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BufferBuilderStorage.class)
 public class BufferBuilderStorageMixin {
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/BlockBufferBuilderPool;allocate(I)Lnet/minecraft/client/render/chunk/BlockBufferBuilderPool;"))
-    private BlockBufferBuilderPool sodium$doNotAllocateChunks(int i) {
-        return new NonStoringBuilderPool();
-    }
+
+private static NonStoringBuilderPool s_nonStoringBuilderPool;
+
+@Inject(method = "<init>", at = @At("RETURN"))
+private void sodium$initializeNonStoringBuilderPool(int i, CallbackInfo ci) {
+    s_nonStoringBuilderPool = new NonStoringBuilderPool();
+}
+
+@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/BlockBufferBuilderPool;allocate(I)Lnet/minecraft/client/render/chunk/BlockBufferBuilderPool;"))
+private BlockBufferBuilderPool sodium$doNotAllocateChunks(int i) {
+    return s_nonStoringBuilderPool;
+   }
 }
